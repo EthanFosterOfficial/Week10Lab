bf4fae0928e6f3066f5148cbf4d625b5a1ce86f1
727045 <727045@SAIT224402IT.ACDM.DS.SAIT.CA>
2020-11-14 13:31:49 -0700

    AdminFilter working with user and role validation.
    tested with non-admin, no user, and admin user

--- src/java/filters/AdminFilter.java
+++ src/java/filters/AdminFilter.java
@@ -14,38 +14,58 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import models.Role;
+import models.User;
 import services.AccountService;
 
-public class AdminFilter implements Filter {
-    
+public class AdminFilter implements Filter
+{
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
             FilterChain chain)
-            throws IOException, ServletException {
+            throws IOException, ServletException
+    {
 
-            // code that is executed before the servlet
-            HttpServletRequest httpRequest = (HttpServletRequest)request;
-            HttpSession session = httpRequest.getSession();
-            String email = (String)session.getAttribute("email");
-           
-            if (!email.contains("admin")) {
-                HttpServletResponse httpResponse = (HttpServletResponse)response;
-                httpResponse.sendRedirect("notes");
-                return;
-            }
-            
-            chain.doFilter(request, response); // execute the servlet
-            
-            // code that is executed after the servlet
-        
+        // code that is executed before the servlet
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpSession session = httpRequest.getSession();
+        String email = (String) session.getAttribute("email");
+        String password = (String) session.getAttribute("password");
+        User user = new AccountService().login(email,password);
+
+        if (user == null)
+        {
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            httpResponse.sendRedirect("login");
+            return;
+        }
+        Role role = user.getRole();
+        if (role.getRoleId() == 2)
+        {
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            httpResponse.sendRedirect("notes");
+            return;
+        }
+
+//            if (!email.contains("admin")) {
+//                HttpServletResponse httpResponse = (HttpServletResponse)response;
+//                httpResponse.sendRedirect("notes");
+//                return;
+//            }
+//            
+        chain.doFilter(request, response); // execute the servlet
+
+        // code that is executed after the servlet
     }
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-            }
+    public void init(FilterConfig filterConfig) throws ServletException
+    {
+    }
 
     @Override
-    public void destroy() {
-       
+    public void destroy()
+    {
+
     }
 }
